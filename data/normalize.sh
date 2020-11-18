a2cefca97cbf45d8f15dc2032029a6202e367bc9
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-11-18 22:51:51 +0100

    Remove dependency on rename from normalization script

--- data/normalize.sh
+++ data/normalize.sh
@@ -4,5 +4,7 @@ rm sprites/gen5-normalize -rf || true
 cp -r sprites/gen5 sprites/gen5-normalize
 cd sprites/gen5-normalize
 
-mogrify -monitor -gravity center -extent 128x128 -background green1 -format PNG8 *.png
-rename 'PNG8' 'png' *.PNG8
+mogrify -monitor -gravity center -extent 128x128 -background green1 -format PNG24 *.png
+for file in *.PNG8; do
+    mv -- "$file" "${file%PNG8}"png
+done
