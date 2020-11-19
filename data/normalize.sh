7f2ec9cf4c66f5bda5e6e7b4c07758f2cb915b89
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-11-19 21:29:58 +0100

    Add a step to remove non-Pokemon sprites to normalization

--- data/normalize.sh
+++ data/normalize.sh
@@ -5,6 +5,11 @@ cp -r sprites/gen5 sprites/gen5-normalize
 cd sprites/gen5-normalize
 
 mogrify -monitor -gravity center -extent 128x128 -background green1 -format PNG24 *.png
+# Remove non-Pokemon sprites
+rm 0.png
+rm missingno.png
+rm pokestar*.png
+
 for file in *.PNG24; do
     mv -- "$file" "${file%PNG24}"png
 done
